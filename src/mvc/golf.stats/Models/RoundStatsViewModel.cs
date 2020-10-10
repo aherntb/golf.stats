b8981c4fa6ea8de9bd06a3adf587770bbd1cb472
Brian Ahern <aherntb@gmail.com>
2020-10-10 18:17:04 +0100

    Add viewmodel

---
+++ src/mvc/golf.stats/Models/RoundStatsViewModel.cs
@@ -0,0 +1,45 @@
+namespace golf.stats.Models
+{
+    public class RoundStatsViewModel
+    {
+        public int[] Score { get; set; }
+        public GolfBag[] TeeShotClub { get; set; }
+        public GolfBag[] ApproachClub { get; set; }
+        public YesNo[] FairwayInReg { get; set; }
+        public YesNo[] GreenInReg { get; set; }
+        public YesNoNa[] Scrambling { get; set; }
+        public int[] NumOfPutts { get; set; }
+
+    }
+
+    public enum YesNo
+    {
+        Yes,
+        No
+    }
+
+    public enum YesNoNa
+    {
+        Yes,
+        No,
+        NA
+    }
+
+    public enum GolfBag
+    {
+        Driver,
+        ThreeWood,
+        FiveWood,
+        TwentyOneDegree,
+        TwentyThreeDegree,
+        FourIron,
+        FiveIron,
+        SixIron,
+        SevenIron,
+        EightIron,
+        NineIron,
+        PitchingWedge,
+        GapWedge,
+        LobWedge
+    }
+}
